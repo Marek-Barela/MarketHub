bc7f1926b54e8327a7493becb1b2d2d57455983a
Marek Bareła <marek.barela@wp.pl>
2021-03-23 09:46:44 +0100

    sass installed, header styling update, google fonts added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,14 @@
 import React, { FC } from 'react';
+import logo from '../../assets/logo.png';
+import './Header.styles.scss';
 
 const Header: FC = () => {
-	return <header>header</header>;
+	return (
+		<header className='header'>
+			<img src={logo} alt='MarketHub' />
+			<div className='nav'></div>
+		</header>
+	);
 };
 
 export default Header;
