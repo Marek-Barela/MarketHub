291073f3007ad4586d88e89f91828e66466fe7eb
Marek Bareła <marek.barela@wp.pl>
2021-03-23 17:28:40 +0100

    StockList and StockListItem component structure and styling update

--- src/components/StockList/StockList.tsx
+++ src/components/StockList/StockList.tsx
@@ -7,6 +7,7 @@ import { createStructuredSelector } from 'reselect';
 
 import { RootState } from '../../redux/root-reducer';
 import { ListOfStocksResponse } from '../../redux/stock/stock.model';
+import './StockList.styles.scss';
 
 interface StateProps {
 	stockList: ListOfStocksResponse;
@@ -16,24 +17,36 @@ type Props = StateProps;
 
 const StockList: FC<Props> = ({ stockList: { bestMatches } }) => {
 	return (
-		<div>
-			{bestMatches.map((item, index) => {
-				const symbol = item['1. symbol'];
-				const name = item['2. name'];
-				const region = item['4. region'];
-				const currency = item['8. currency'];
+		<table className='table'>
+			<thead>
+				<tr>
+					<th>Country</th>
+					<th>Name</th>
+					<th>Instrument Type</th>
+					<th>Currency</th>
+				</tr>
+			</thead>
+			<tbody>
+				{bestMatches.map((item, index) => {
+					const symbol = item['1. symbol'];
+					const name = item['2. name'];
+					const type = item['3. type'];
+					const region = item['4. region'];
+					const currency = item['8. currency'];
 
-				return (
-					<StockListItem
-						key={index}
-						symbol={symbol}
-						name={name}
-						region={region}
-						currency={currency}
-					/>
-				);
-			})}
-		</div>
+					return (
+						<StockListItem
+							key={index}
+							symbol={symbol}
+							type={type}
+							name={name}
+							region={region}
+							currency={currency}
+						/>
+					);
+				})}
+			</tbody>
+		</table>
 	);
 };
 
--- src/components/StockListItem/StockListItem.tsx
+++ src/components/StockListItem/StockListItem.tsx
@@ -1,16 +1,28 @@
 import React, { FC } from 'react';
+import './StockListItem.styles.scss';
 
 interface ParentProps {
 	symbol: String;
 	name: String;
+	type: String;
 	region: String;
 	currency: String;
 }
 
 type Props = ParentProps;
 
-const StockListItem: FC<Props> = ({ symbol, name, region, currency }) => {
-	return <div>item</div>;
+const StockListItem: FC<Props> = ({ symbol, name, type, region, currency }) => {
+	return (
+		<tr className='listitem-row'>
+			<td className='region-row'>{region}</td>
+			<td className='name-row'>
+				<span>{symbol}</span>
+				<p>{name}</p>
+			</td>
+			<td className='type-row'>{type}</td>
+			<td className='currency-row'>{currency}</td>
+		</tr>
+	);
 };
 
 export default StockListItem;
