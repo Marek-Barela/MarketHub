dcbc5eb1566a7d18b9a6b0315ba69269deae8302
Marek Bareła <marek.barela@wp.pl>
2021-03-24 08:27:15 +0100

    graph-selector update

--- src/redux/graph/graph-selectors.ts
+++ src/redux/graph/graph-selectors.ts
@@ -1,13 +1,22 @@
 import { createSelector } from 'reselect';
 import { RootState } from '../root-reducer';
+import { selectStockList } from '../stock/stock.selector';
 
 const selectGraphData = (state: RootState) => {
 	return state.graph;
 };
 
 export const selectCurrentGraphSymbol = createSelector(
-	[selectGraphData],
-	(graph) => graph.dailyData['Meta Data']['2. Symbol']
+	[selectGraphData, selectStockList],
+	(graph, stock) => {
+		const symbol = graph.dailyData['Meta Data']['2. Symbol'] || '';
+		const name =
+			stock.bestMatches.find(
+				(stockItem) => stockItem['1. symbol'] === symbol
+			)?.['2. name'] || '';
+
+		return `${symbol} - ${name}`;
+	}
 );
 
 export const selectGraphDailyData = createSelector([selectGraphData], (graph) =>
