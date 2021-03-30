2673c928313c69a62271f51cf13ce5c04cf9901e
Marek Bareła <marek.barela@wp.pl>
2021-03-30 13:23:42 +0200

    index css changed to scss

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import './index.scss';
 
 import reportWebVitals from './reportWebVitals';
 
