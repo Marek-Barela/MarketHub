358d7af2a2b060b80e53a0e7bdb33f5c96667294
Marek Bareła <marek.barela@wp.pl>
2021-05-01 21:40:25 +0200

    package vulnerablitie update

--- src/redux/graph/graph-api.ts
+++ src/redux/graph/graph-api.ts
@@ -1,10 +1,8 @@
 import { GraphInterface } from './graph-model';
 import axios from 'axios';
 
-export const getStockDailyData = async (
-	symbol: string
-): Promise<GraphInterface> => {
+export const getStockDailyData = async (symbol: string): Promise<GraphInterface> => {
 	return await axios(
-		`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${process.env.REACT_APP_API_KEY}`
+		`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey={process.env.REACT_APP_API_KEY}`
 	).then((res) => res.data);
 };
